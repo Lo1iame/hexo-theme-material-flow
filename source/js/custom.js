6d2404ab298525885006d20eafc1f13eabc2264c
loriame <1007574921@qq.com>
2021-09-14 17:31:37 +0800

    修改主题

---
+++ source/js/custom.js
@@ -0,0 +1,9 @@
+(function ($) {
+  // 判断当前地址
+  if (location.pathname === "/dataset/") {
+    // 如果没有 #data 标识，则认为进入首页
+    if (location.hash !== "#data") {
+      location.href = location.origin + "/dataset/main/";
+    }
+  }
+})(jQuery);
